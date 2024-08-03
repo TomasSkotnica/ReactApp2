7d2186def38683de6d3f33b2956b6012213e8990
Tomas Skotnica <tomas.skotnica@seznam.cz>
2024-08-03 21:16:03 +0200

    Generic -  display ciselnik in list of buttons

--- reactapp2.client/src/others/Generic.tsx
+++ reactapp2.client/src/others/Generic.tsx
@@ -1,20 +1,51 @@
+import React from 'react';
+
 import { useState } from 'react';
 
+type IdNameItem = {
+    id: number;
+    name:string;
+}
+
+type GenericProps<T> = { options: T[], selectedO: T, onOptionClick: (theme: T) => void };
+
+function GenericIdName<IdNameItem>({ options, selectedO, onOptionClick }: GenericProps<IdNameItem>) {
+    console.log("GenericIdName returns ...");
+    return (
+        <ul>
+            {options.map((theme, index) => (
+                <li key={index}>
+                    <button
+                        onClick={() => onOptionClick(theme)}
+                        className={theme.id === selectedO.id ? "font-bold" : ""}
+                    >
+                        {theme.name}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
 export default function Themes() {
     console.log("Themes started");
     const tOpt = ["a", "b", "c"];
     const [selectedT, setSelectedT] = useState(tOpt[0]);
+    const Gens: IdNameItem[] = [];
+    const i1: IdNameItem = { id: 1, name:"Form"};
+    Gens.push(i1);
+    const i2: IdNameItem = { id: 2, name: "Deskt" };
+    Gens.push(i2);
+    const [selIt, setSelIt] = useState(Gens[0]);
+
     console.log("Themes returns ...");
     return (<>
+        <GenericIdName options={Gens} selectedO={selIt} onOptionClick={(theme) => setSelIt(theme)} />
+        <p>Selected theme: {selIt.name}</p>
         <Generic options={tOpt} selectedO={selectedT} onOptionClick={(theme) => setSelectedT(theme)} />
-        <p>Selected theme: {selectedT}</p></>
-    );
+        <p>Selected theme: {selectedT}</p>
+    </>);
 }
 
-type GenericProps<T> = { options: T[], selectedO: T, onOptionClick: (theme:T)=>void };
-
-// function Generic(props: { fOpt: string[] }) { // misto props: muzeme psat {mojeOpt} a pouzit mojeOpt.map()
-// f*** znamena function v tomto priklade
 function Generic<T extends React.ReactNode>({ options, selectedO, onOptionClick }: GenericProps<T>) {
     console.log("Generic returns ...");
     return (
@@ -31,5 +62,5 @@ function Generic<T extends React.ReactNode>({ options, selectedO, onOptionClick 
             ))}
         </ul>
     );
-
 }
+
